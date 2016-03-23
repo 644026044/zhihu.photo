24ac411c9a6460ebaebf2f844ccd262ca4ad2d87
xlzd <i@xlzd.me>
2016-03-24 00:02:13 +0800

    crawler config

---
+++ crawler/config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+FILE_PATH = '/data/zhihu.photo'
+MONGO_HOST, MONGO_PORT = '127.0.0.1', 27017
+DB = 'zhihu_photo'
+ANSWER_COLL = 'answer'
+QUESTION_COLL = 'question'
