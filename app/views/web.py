59001163d84bec77fd5508441ed7560b79c7e929
xlzd <i@xlzd.me>
2016-03-26 14:10:35 +0800

    web

--- app/views/web.py
+++ app/views/web.py
@@ -8,4 +8,17 @@ web = Blueprint('web', __name__)
 
 @web.route('/')
 def index():
-    return 'hello, world'
+    data = {
+        'active': 'index',
+        'title': u'首页'
+    }
+    return render_template('index.html', **data)
+
+
+@web.route('/about')
+def about():
+    data = {
+        'active': 'about',
+        'title': u'关于'
+    }
+    return render_template('about.html', **data)
