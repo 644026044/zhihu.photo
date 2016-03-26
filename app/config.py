51a0f676056bc1f629577902389bbd8b12349314
xlzd <i@xlzd.me>
2016-03-26 16:12:43 +0800

    config file update

--- app/config.py
+++ app/config.py
@@ -2,3 +2,11 @@
 # encoding=utf-8
 
 SECRET_KEY = 'what the f*ck'
+
+FILE_PATH = '/data/zhihu.photo'
+MONGO_HOST, MONGO_PORT = '127.0.0.1', 27017
+DB = 'zhihu_photo'
+ANSWER_COLL = 'answer'
+QUESTION_COLL = 'question'
+
+PAGE_SIZE = 5
