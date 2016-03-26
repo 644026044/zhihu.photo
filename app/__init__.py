83e4026a44b5cc476785f352693a91d8df29bd0f
xlzd <i@xlzd.me>
2016-03-26 16:11:32 +0800

    add api blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,10 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = config.SECRET_KEY
 
-    from .views.web import web as web_blueprint
-    app.register_blueprint(web_blueprint)
+    from .views.web import web
+    app.register_blueprint(web)
+
+    from .views.api import api
+    app.register_blueprint(api)
 
     return app
