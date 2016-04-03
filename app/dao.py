dbdbbb347b80be420b33220ae3900e920cbedb59
xlzd <i@xlzd.me>
2016-04-03 13:55:11 +0800

    sqlite for /nobody

--- app/dao.py
+++ app/dao.py
@@ -2,9 +2,14 @@
 # encoding=utf-8
 
 from pymongo import MongoClient
+import sqlite3
+import time
 from xtls.timeparser import now
 
 from config import *
+
+SQLITE = sqlite3.connect(SQLITE_DB_PATH)
+SQLITE_SELETE = 'SELECT * FROM imgs ORDER BY id DESC LIMIT %d OFFSET {start}' % PAGE_SIZE
 
 MONGO = MongoClient(MONGO_HOST, MONGO_PORT)
 
@@ -28,6 +33,26 @@ def insert(coll, data):
     )
 
 
+def get_qbcr_imgs(page):
+    sql = SQLITE_SELETE.format(start=PAGE_SIZE * (page - 1))
+    c = SQLITE.cursor()
+    result = [item for item in c.execute(sql)]
+    c.close()
+    return result
+
+
+count = last_change_time = 0
+
+
+def get_qbcr_count():
+    global count, last_change_time
+    if time.time() - last_change_time > 86400:
+        c = SQLITE.cursor()
+        count = c.execute('select count(id) from imgs').next()[0]
+        c.close()
+        last_change_time = time.time()
+    return count
+
+
 if __name__ == '__main__':
-    for item in select(ANSWER_COLL, {}):
-        print item['updateTime']
+    print get_qbcr_imgs(1)
